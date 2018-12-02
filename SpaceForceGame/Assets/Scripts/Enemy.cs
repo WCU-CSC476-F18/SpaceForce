e83dc111e85c381194d6915b369d9826d7904a98
hahas <38119454+hab91@users.noreply.github.com>
2018-12-01 22:54:12 -0500

    try to add bullet to enemy ship

--- SpaceForceGame/Assets/Scripts/Enemy.cs
+++ SpaceForceGame/Assets/Scripts/Enemy.cs
@@ -8,21 +8,52 @@ public class Enemy : MonoBehaviour {
     public float moveSpeed = 2f;
     int wayPointIndex = 0;
 
+    //code from martinel////////////////////////////////
+    GameObject muzzleFlash;
+    private bool hasFired = false;
 
+    public bool canMove = true;
+    public float shieldHealth = 50;
+    public GameObject bullet;
+    public float bulletSpeed = 30f; // speed of bullets
+    /// ////////////////////////////////////////////////
+    
 
     public void Start()
     {
         wayPoints = config.GetWayPoints();
         transform.position = wayPoints[wayPointIndex].transform.position;
+        muzzleFlash = this.transform.Find("MuzzleFlash").gameObject;
+
     }
 
     public void Update()
     {
-        MoveEnemy();
+        if (canMove)
+            MoveEnemy();
     }
 
     public void MoveEnemy()
     {
+        // Handles user movement and firing
+
+        // Listen for player input and FIRE
+        if (Input.GetButton("Fire1"))
+        {
+            muzzleFlash.SetActive(true);
+            if (!hasFired)
+            {
+                fire();
+                Invoke("resetFire", 0.1f);
+                hasFired = true;
+            }
+        }
+        else
+        {
+             muzzleFlash.SetActive(false);
+        }
+           
+
         if (wayPointIndex <= wayPoints.Count - 1)
         {
             var targetPosition = wayPoints[wayPointIndex].transform.position;
@@ -38,5 +69,11 @@ public class Enemy : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+    }
+    private void fire()
+    {
+        GameObject shot = Instantiate(bullet, muzzleFlash.transform.position, bullet.transform.rotation);
+        Rigidbody2D rb = shot.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, bulletSpeed);
     }
 }
